c3b66a93a3b3b37b4b19ebea981f1afa3cbdb5c2
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-09 01:11:43 +0200

    ng g c add-trip --module=trip

--- client/src/app/trip/trip.module.ts
+++ client/src/app/trip/trip.module.ts
@@ -4,12 +4,14 @@ import { TripComponent } from './trip/trip.component';
 import { AttractionComponent } from './attraction/attraction.component';
 import { MaterialModule } from '../material/material.module';
 import { TripListComponent } from './trip-list/trip-list.component';
+import { AddTripComponent } from './add-trip/add-trip.component';
 
 @NgModule({
   declarations: [
     TripComponent,
     AttractionComponent,
-    TripListComponent
+    TripListComponent,
+    AddTripComponent
   ],
   imports: [
     CommonModule,
