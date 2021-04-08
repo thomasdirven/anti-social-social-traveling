cefe11b54c88207931e89da7293808d11b19ba36
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-08 21:27:15 +0200

    ng g c attraction

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { TripComponent } from './trip/trip.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AttractionComponent } from './attraction/attraction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TripComponent
+    TripComponent,
+    AttractionComponent
   ],
   imports: [
     BrowserModule,
