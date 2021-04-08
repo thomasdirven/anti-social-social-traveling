cf9975bbbdefa4f076a290af11c9af381922266e
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-08 02:13:56 +0200

    ng add @angular/material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TripComponent } from './trip/trip.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TripComponent } from './trip/trip.component';
     TripComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
