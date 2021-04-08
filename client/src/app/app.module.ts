01d12d7d9c9417a2208a7b262021b0a9961e1707
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-08 22:06:20 +0200

    Added trip.module.ts (+moved trip and attraction into trip/)
    BugFix scss file path

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,17 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { TripComponent } from './trip/trip.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AttractionComponent } from './attraction/attraction.component';
 import { TripModule } from './trip/trip.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    TripComponent,
-    AttractionComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
--- client/src/app/trip/trip.module.ts
+++ client/src/app/trip/trip.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TripComponent } from './trip/trip.component';
+import { AttractionComponent } from './attraction/attraction.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TripComponent,
+    AttractionComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    TripComponent
   ]
 })
 export class TripModule { }
