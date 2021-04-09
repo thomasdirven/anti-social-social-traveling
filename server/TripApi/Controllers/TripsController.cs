08433ff1da2eb2710305d49621b5def6da7cf753
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-09 16:18:34 +0200

    Add GET Trips by ID (+rename for naming conventions TripsController)
    TripController -> TripsController
    squash
    
    
    Another sqaush
    
    As a wise man once said: "kzijn aant sauzen" - Lieven Smits

--- server/TripApi/Controllers/TripsController.cs
+++ server/TripApi/Controllers/TripsController.cs
@@ -7,11 +7,11 @@ namespace TripApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class TripController : ControllerBase
+    public class TripsController : ControllerBase
     {
         private readonly ITripRepository _tripRepository;
 
-        public TripController(ITripRepository context)
+        public TripsController(ITripRepository context)
         {
             _tripRepository = context;
         }
@@ -21,6 +21,15 @@ namespace TripApi.Controllers
         public IEnumerable<Trip> GetTrips()
         {
             return _tripRepository.GetAll().OrderBy(r => r.City);
+        }
+
+        // GET: api/Trips/id
+        [HttpGet("{id}")]
+        public ActionResult<Trip> GetTrip(int id)
+        {
+            Trip trip = _tripRepository.GetBy(id);
+            if (trip == null) return NotFound();
+            return trip;
         }
     }
 }
