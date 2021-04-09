6e8f3b24a0f10dc651c3726fd537bac29f14638c
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-09 04:40:35 +0200

    Switched from Swashbuckle to NSwag
    Don't know which one is better, but I'm using NSwag for now
    handy links for swagger
    
    Nswag already uses OpenAPI 3.0, SwashBuckle still in beta

--- server/tripApi/Startup.cs
+++ server/tripApi/Startup.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
+//using Microsoft.OpenApi.Models;
 
 namespace tripApi
 {
@@ -28,10 +28,23 @@ namespace tripApi
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+
+            // <PackageReference Include="Swashbuckle.AspNetCore" Version="5.6.3" />
+            // Swashbuckle.AspNetCore (replaced by NSwag)
+            //services.AddSwaggerGen(c =>
+            //{
+            //    c.SwaggerDoc("v1", new OpenApiInfo { Title = "tripApi", Version = "v1" });
+            //});
+            // Register the Swagger services (NSwag)
+            services.AddOpenApiDocument(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "tripApi", Version = "v1" });
-            });
+                c.DocumentName = "tripapidocs";
+                c.Title = "Trip API";
+                c.Version = "v1";
+                c.Description = "The Trip API documentation description.";
+            }); //for OpenAPI 3.0 else AddSwaggerDocument();
+
+            services.AddCors(options => options.AddPolicy("AllowAllOrigins", builder => builder.AllowAnyOrigin()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,11 +53,16 @@ namespace tripApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "tripApi v1"));
+                // Swashbuckle.AspNetCore (replaced by NSwag)
+                //app.UseSwagger();
+                //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "tripApi v1"));
             }
 
             app.UseHttpsRedirection();
+
+            // Register the Swagger generator and the Swagger UI middlewares
+            app.UseOpenApi(); // https://localhost:5001/swagger/tripapidocs/swagger.json
+            app.UseSwaggerUi3(); // https://localhost:5001/swagger/
 
             app.UseRouting();
 
