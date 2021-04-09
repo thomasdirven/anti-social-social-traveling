826705d800d54fc2457344649cb417b7e0ebf8c0
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-09 02:12:59 +0200

    ng generate pipe TripFilter --module=trip

--- client/src/app/trip/trip.module.ts
+++ client/src/app/trip/trip.module.ts
@@ -5,13 +5,15 @@ import { AttractionComponent } from './attraction/attraction.component';
 import { MaterialModule } from '../material/material.module';
 import { TripListComponent } from './trip-list/trip-list.component';
 import { AddTripComponent } from './add-trip/add-trip.component';
+import { TripFilterPipe } from './trip-filter.pipe';
 
 @NgModule({
   declarations: [
     TripComponent,
     AttractionComponent,
     TripListComponent,
-    AddTripComponent
+    AddTripComponent,
+    TripFilterPipe
   ],
   imports: [
     CommonModule,
