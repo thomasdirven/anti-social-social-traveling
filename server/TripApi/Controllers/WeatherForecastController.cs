61fef68524fb7e591e97802b481189b2a4b5bf33
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-09 05:20:38 +0200

    Renamed namespace to TripApi
    git mv casesensitive tmp
    git mv tmp CaseSensitive
    Changes in lauchSettings.json

--- server/TripApi/Controllers/WeatherForecastController.cs
+++ server/TripApi/Controllers/WeatherForecastController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace tripApi.Controllers
+namespace TripApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
--- server/TripApi/WeatherForecast.cs
+++ server/TripApi/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace tripApi
+namespace TripApi
 {
     public class WeatherForecast
     {
