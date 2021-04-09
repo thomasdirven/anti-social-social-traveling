c7ed8732d025b485fe9a7bbc3f786ef21f69dad0
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-09 16:54:55 +0200

    Added StatusCodes (Status500InternalServerError)

--- server/TripApi/Controllers/TripsController.cs
+++ server/TripApi/Controllers/TripsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using TripApi.Models;
@@ -27,46 +29,74 @@ namespace TripApi.Controllers
         [HttpGet("{id}")]
         public ActionResult<Trip> GetTrip(int id)
         {
-            Trip trip = _tripRepository.GetBy(id);
-            if (trip == null) return NotFound();
-            return trip;
+            try
+            {
+                Trip trip = _tripRepository.GetBy(id);
+                if (trip == null) return NotFound();
+                return trip;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         // POST: api/Trips
         [HttpPost]
         public ActionResult<Trip> PostTrip(Trip trip)
         {
-            _tripRepository.Add(trip);
-            _tripRepository.SaveChanges();
+            try
+            {
+                _tripRepository.Add(trip);
+                _tripRepository.SaveChanges();
 
-            // 201 - link to get new Trip
-            return CreatedAtAction(nameof(GetTrip), new { id = trip.Id }, trip);
+                // 201 - link to get new Trip
+                return CreatedAtAction(nameof(GetTrip), new { id = trip.Id }, trip);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         // PUT: api/Trips/id
         [HttpPut("{id}")]
         public IActionResult PutTrip(int id, Trip trip)
         {
-            // 404 if trip with id doesn't exist
-            if (_tripRepository.GetBy(id) == null) return NotFound();
-            // 400 (Bad Request) id’s don’t match
-            if (id != trip.Id) return BadRequest();
-            
-            _tripRepository.Update(trip);
-            _tripRepository.SaveChanges();
-            // 204(No Content) when ModelState validation fails or 200+Trip
-            return NoContent();
+            try
+            {
+                // 404 if trip with id doesn't exist
+                if (_tripRepository.GetBy(id) == null) return NotFound();
+                // 400 (Bad Request) id’s don’t match
+                if (id != trip.Id) return BadRequest();
+
+                _tripRepository.Update(trip);
+                _tripRepository.SaveChanges();
+                // 204(No Content) when ModelState validation fails or 200+Trip
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
 
         // DELETE: api/Trips/id
         [HttpDelete("{id}")]
         public IActionResult DeleteTrip(int id)
         {
-            Trip trip = _tripRepository.GetBy(id);
-            if (trip == null) return NotFound();
-            _tripRepository.Delete(trip);
-            _tripRepository.SaveChanges();
-            return NoContent();
+            try
+            {
+                Trip trip = _tripRepository.GetBy(id);
+                if (trip == null) return NotFound();
+                _tripRepository.Delete(trip);
+                _tripRepository.SaveChanges();
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
         }
     }
 }
