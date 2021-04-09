dee897c474c230da62232cec080313401c635d0d
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-09 19:51:21 +0200

    Added DTOs (Data Transfer Objects)
    Comparable with ViewModels

---
+++ server/TripApi/DTOs/AttractionDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TripApi.DTOs
+{
+    public class AttractionDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public double? Budget { get; set; }
+    }
+}
---
+++ server/TripApi/DTOs/TripDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TripApi.DTOs
+{
+    public class TripDTO
+    {
+        [Required]
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int MinDays { get; set; }
+        public int MaxDays { get; set; }
+
+        public IList<AttractionDTO> Attractions { get; set; }
+    }
+}
