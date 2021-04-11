ad58e206cc965ec065c5c2a78565f6bafbbd4b2c
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-11 23:42:35 +0200

    Google Maps displayed with agm

--- client/src/app/trip/gmap/gmap.component.ts
+++ client/src/app/trip/gmap/gmap.component.ts
@@ -1,15 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
+// No longer used in latest version of agm -> using googl.maps
+//import { MouseEvent } from '@agm/core';
+
+// just an interface for type safety.
+interface marker {
+  lat: number;
+  lng: number;
+  label?: string;
+  draggable: boolean;
+}
 
 @Component({
   selector: 'app-gmap',
   templateUrl: './gmap.component.html',
-  styleUrls: ['./gmap.component.scss']
+  styleUrls: ['./gmap.component.scss'],
 })
 export class GmapComponent implements OnInit {
+  constructor(private zone: NgZone) {}
 
-  constructor() { }
+  // google maps zoom level
+  zoom: number = 8;
 
-  ngOnInit(): void {
+  // initial center position for the map
+  lat: number = 51.673858;
+  lng: number = 7.815982;
+
+  clickedMarker(label: string, index: number) {
+    console.log(`clicked the marker: ${label || index}`);
   }
 
+  mapClicked($event: google.maps.MouseEvent): void {
+    this.markers.push({
+      lat: $event.latLng.lat(),
+      lng: $event.latLng.lng(),
+      draggable: true,
+    });
+  }
+
+  markerDragEnd(m: marker, $event: google.maps.MouseEvent) {
+    console.log('dragEnd', m, $event);
+  }
+
+  markers: marker[] = [
+    {
+      lat: 51.673858,
+      lng: 7.815982,
+      label: 'A',
+      draggable: true,
+    },
+    {
+      lat: 51.373858,
+      lng: 7.215982,
+      label: 'B',
+      draggable: false,
+    },
+    {
+      lat: 51.723858,
+      lng: 7.895982,
+      label: 'C',
+      draggable: true,
+    },
+  ];
+
+  ngOnInit(): void {}
+
+  map: google.maps.Map;
+  mapClickListener;
+
+  public mapReadyHandler(map: google.maps.Map): void {
+    this.map = map;
+    this.mapClickListener = this.map.addListener(
+      'click',
+      (e: google.maps.MouseEvent) => {
+        this.zone.run(() => {
+          // Here we can get correct event
+          console.log(e.latLng.lat(), e.latLng.lng());
+        });
+      }
+    );
+  }
+
+  public ngOnDestroy(): void {
+    if (this.mapClickListener) {
+      this.mapClickListener.remove();
+    }
+  }
 }
