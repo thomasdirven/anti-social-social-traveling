0501da143b9777c959bd914342b0d82e890ca3fe
Thomas Dirven <thomas.dirven@student.hogent.be>
2021-04-13 00:43:25 +0200

    Form add tourist attraction

--- client/src/app/material/material.module.ts
+++ client/src/app/material/material.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatListModule } from '@angular/material/list';
+import { MatListModule, MatSelectionList } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [],
@@ -23,6 +25,8 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     MatInputModule,
     MatButtonModule,
     MatProgressSpinnerModule,
+    MatOptionModule,
+    MatSelectModule,
   ],
   exports: [
     BrowserAnimationsModule,
@@ -34,6 +38,8 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
     MatInputModule,
     MatButtonModule,
     MatProgressSpinnerModule,
+    MatOptionModule,
+    MatSelectModule,
   ],
 })
 export class MaterialModule {}
--- client/src/app/trip/add-trip/add-trip.component.ts
+++ client/src/app/trip/add-trip/add-trip.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Attraction } from '../attraction.model';
 import { Trip } from '../trip.model';
 
@@ -9,28 +9,47 @@ import { Trip } from '../trip.model';
   styleUrls: ['./add-trip.component.scss']
 })
 export class AddTripComponent implements OnInit {
+  public readonly attractionTypes = ['Historic Building', 'Park', 'Square', 'Museum', 'Other' ];
+
   public tripFG: FormGroup;
   @Output() public newTrip = new EventEmitter<Trip>();
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.tripFG = new FormGroup({
-      city: new FormControl('Rome', [Validators.required, Validators.minLength(5)]),
+    this.newFormGroup();    
+  }
+
+  newFormGroup() {
+    this.tripFG = this.fb.group({
+      city: ['Rome', [Validators.required, Validators.minLength(3)]],      
       // country: new FormControl('Rome'),
       // startDate: new FormControl('Rome'),
       // endDate: new FormControl('Rome'),
       // minDays: new FormControl('Rome'),
       // maxDays: new FormControl('Rome'),
-    })
+      attractions: this.fb.array([this.createAttractions() ])
+    });
   }
 
-  onSubmit(): boolean {
+  createAttractions(): FormGroup {
+    return this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      type: [''],
+      budget: ['']
+    });
+  }
+
+  onSubmit() {
     console.log(this.tripFG);
-    const attraction = new Attraction("Test", "Test", 5)
-    const trip = new Trip(this.tripFG.value.city, 'UNKOWN', new Date(), new Date(Date.now() + 12096e5), 2, 5, [attraction]);
+    let attractions = this.tripFG.value.attractions.map(Attraction.fromJSON);
+    attractions = attractions.filter(att => att.name.length > 2);
+    //const attraction = new Attraction("Test", "Test", 5);
+    const trip = new Trip(this.tripFG.value.city, 'UNKOWN', new Date(), new Date(Date.now() + 12096e5), 2, 5, attractions);
     console.log(trip);
     this.newTrip.emit(trip);
-    return false;
+
+    // reset form with new form group after submit    
+    this.newFormGroup();  
   }
 
   getErrorMessages(errors: any): string {
